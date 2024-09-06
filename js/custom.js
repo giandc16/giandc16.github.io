bc5106f7c752c8d26234952cf3e97f618037184f
giandc16 <giandc16@gmail.com>
2024-09-06 10:29:30 +0800

    Signed-off-by: giandc16 <giandc16@gmail.com>

--- js/custom.js
+++ js/custom.js
@@ -1,50 +1,21 @@
 ;
 (function($, window, document, undefined) {
     'use strict';
-    var $winW = function() {
-        return $(window).width();
-    };
-    var $winH = function() {
-        return $(window).height();
-    };
-    var $screensize = function(element) {
-        $(element).width($winW()).height($winH());
-    };
-    var screencheck = function(mediasize) {
-        if (typeof window.matchMedia !== "undefined") {
-            var screensize = window.matchMedia("(max-width:" + mediasize + "px)");
-            if (screensize.matches) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            if ($winW() <= mediasize) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    };
+
     $(document).ready(function() {
         $(window).on('load', function() {
             $('.preloader').fadeOut();
-            $('.animated-row').each(function() {
-                var $this = $(this);
-                $this.find('.animate').each(function(i) {
-                    var $item = $(this);
-                    var animation = $item.data('animate');
-                    $item.on('inview', function(event, isInView) {
-                        if (isInView) {
-                            setTimeout(function() {
-                                $item.addClass('animated ' + animation).removeClass('animate');
-                            }, i * 50);
-                        } else if (!screencheck(767)) {
-                            $item.removeClass('animated ' + animation).addClass('animate');
-                        }
-                    });
-                });
-            });
+
+
+            $.scrollify({
+                section: ".section",
+          
+                sectionName:false,
+                scrollSpeed: 300,
+                setHeights: false,
+
+
+              });
         });
         if ($('.facts-list').length) {
             $('.facts-list').owlCarousel({
